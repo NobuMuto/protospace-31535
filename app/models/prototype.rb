cff46716abdf84e2fc948fcce04cd0e5435f8735
NobuMuto <tobe.is.todo1214@gmail.com>
2021-01-25 18:28:49 +0900

    投稿機能の実装

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,6 +1,8 @@
 class Prototype < ApplicationRecord
-  belongs_to :users
-  has_many   :users
+  belongs_to :user
+ # has_many   :comments
+  #has_many   :users, through: comments
+
   has_one_attached :image
 
   validates :title, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
     validates :position
   end
 
+  #has_many :comments
+  #has_many :prototypes, through: comments
   has_many :prototypes
-  has_many :prototypes
-
 end
